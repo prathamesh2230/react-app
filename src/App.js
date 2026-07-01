ee64652a7848145e5bafcbea98b280fd50cf573f
patty <pattydev1099@gmail.com>
2026-07-01 10:11:14 +0000

    first Project2833

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React From patty
+          Learn React From patty Devops
         </a>
       </header>
     </div>
